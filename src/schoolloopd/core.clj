afc7ff7d214318c5ee215a814c07a952897c20f0
Michael Lan <michaellan202@gmail.com>
2021-04-22 14:20:32 -0700

    Add main loop function

--- src/schoolloopd/core.clj
+++ src/schoolloopd/core.clj
@@ -1,10 +1,23 @@
 (ns schoolloopd.core
-  (:require [schoolloopd.defaults :as defaults]
+  (:require [schoolloopd.defaults :as d]
             [schoolloopd.fetcher :as fetch]
-            [schoolloopd.updater :as upd]))
+            [schoolloopd.updater :as upd]
+            [schoolloopd.notifier :as n]))
+
+(defn check
+  ([] (check {:prefix d/sl-prefix
+              :username d/sl-username
+              :password d/sl-password}))
+  ([opts]
+   (let [existing (upd/load-data)
+         data (fetch/fetch opts)]
+     (when-not (= existing data)
+       (n/notify {:old existing
+                  :new data}))
+     (upd/save-data data))))
 
 (comment
-  (fetch/fetch {:prefix defaults/sl-prefix
-                :username defaults/sl-username
-                :password defaults/sl-password})
+  (fetch/fetch {:prefix d/sl-prefix
+                :username d/sl-username
+                :password d/sl-password})
   )
